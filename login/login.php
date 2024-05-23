47a98eedb228304ebfd91929daf07af7e77b68e6
dynamolasobrance <olexamaros@gmail.com>
2024-05-23 10:47:39 +0200

    modified:   login/login.php 	modified:   login/register.php

--- login/login.php
+++ login/login.php
@@ -31,5 +31,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "No users registered yet.";
     }
+
+$file = fopen("users.txt", "a") or die("Unable to open file!");
+$data = $_POST['username'] . "," . $_POST['email'] . "," . password_hash($_POST['password'], PASSWORD_DEFAULT) . "\n";
+if (fwrite($file, $data) === FALSE) {
+    echo "Cannot write to file.";
+} else {
+    echo "Record saved.";
+}
+fclose($file);
 }
 ?>
--- login/register.php
+++ login/register.php
@@ -14,5 +14,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     file_put_contents($filename, $userData, FILE_APPEND);
 
     echo "Registration successful. Data saved to $filename";
+
+$file = fopen("users.txt", "a") or die("Unable to open file!");
+$data = $_POST['username'] . "," . $_POST['email'] . "," . password_hash($_POST['password'], PASSWORD_DEFAULT) . "\n";
+if (fwrite($file, $data) === FALSE) {
+    echo "Cannot write to file.";
+} else {
+    echo "Record saved.";
+}
+fclose($file);
 }
 ?>
