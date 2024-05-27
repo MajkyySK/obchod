ded45f0bafa7854cea0240d4b2baf84bf01b7d88
Miso <marak15032007@gmail.com>
2024-05-27 11:46:19 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ function log(){
         wrapper.style.display = "none";
     }
 }
-<<<<<<< HEAD
-=======
+
 
 let slideindex = 1;
 showSlide(slideindex);
@@ -50,4 +49,4 @@ function showSlide(n){
     slides[slideindex-1].style.display = "block";
     dots[slideindex-1].style.display = "inline-block";
 }
->>>>>>> fd32f3d2bce658f0854654a40d1d8c887dea8240
+
